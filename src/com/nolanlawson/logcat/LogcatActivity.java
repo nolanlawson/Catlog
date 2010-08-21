934440a84d92d0e80c8ef6c74b1e68a6d852367b
Nolan Lawson <nolan.lawson@gmail.com>
2010-08-20 20:48:31 -0700

    first release of catlog

--- src/com/nolanlawson/logcat/LogcatActivity.java
+++ src/com/nolanlawson/logcat/LogcatActivity.java
@@ -16,7 +16,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
-import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
--- src/com/nolanlawson/logcat/data/LogFileAdapter.java
+++ src/com/nolanlawson/logcat/data/LogFileAdapter.java
@@ -9,8 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.CheckedTextView;
 import android.widget.TextView;
 
