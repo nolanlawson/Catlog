75901bfc5d90fe20f22ffdd3247bbd75f076c84a
Nolan Lawson <nolan.lawson@gmail.com>
2012-02-08 17:20:14 -0500

    remove unused imports

--- src/com/nolanlawson/logcat/data/SenderAppAdapter.java
+++ src/com/nolanlawson/logcat/data/SenderAppAdapter.java
@@ -2,9 +2,7 @@ package com.nolanlawson.logcat.data;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
